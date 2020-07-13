9e487ca2f26f8834379c6a3f1abc64dbcdad362e
sailyw <1152827086@qq.com>
2020-07-13 14:24:20 +0800

    搜索框以及定位信息显示

--- src/views/Address.vue
+++ src/views/Address.vue
@@ -3,25 +3,44 @@
         <Header :isLeft="true" title="选择收货地址"/>
         <div class="city_search">
             <div class="search">
-                
+                <span class="city">
+                    {{city}}
+                    <i class="fa fa-angle-down"></i>
+                </span>
+                <i class="fa fa-search"></i>
+                <input type="text" v-model="search_val" placeholder="小区/写字楼/学校等">
             </div>
+            <Location :address="address"/>
         </div>
     </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
+import Header from '../components/Header'
+import Location from '../components/Location'
 export default {
     name:'Address',
     components:{
-        Header
+        Header,
+        Location
     },
     data(){
         return{
             city:'', // 当前的城市
             search_val:'' //搜索的值
         }
-    }
+    },
+    computed:{
+        address(){
+            return this.$store.getters.location.formattedAddress;
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+    // console.log(to.params.city + "test");
+    next(vm => {
+      vm.city = to.params.city;
+    });
+  }
 }
 </script>
 
@@ -32,5 +51,45 @@ export default {
     overflow: auto;
     box-sizing: border-box;
     padding-top: 45px;
+}
+.city_search {
+  background-color: #fff;
+  padding: 10px 20px;
+  color: #333;
+}
+
+.search {
+  background-color: #eee;
+  height: 40px;
+  border-radius: 10px;
+  box-sizing: border-box;
+  line-height: 40px;
+}
+.search .city {
+  padding: 0 10px;
+}
+.city i {
+  margin-right: 10px;
+}
+.search input {
+  margin-left: 5px;
+  background-color: #eee;
+  outline: none;
+  border: none;
+}
+
+.area {
+  margin-top: 16px;
+  background: #fff;
+}
+.area li {
+  border-bottom: 1px solid #eee;
+  padding: 8px 16px;
+  color: #aaa;
+}
+.area li h4 {
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 5px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="header">
-      <div class="address_map" @click="$router.push('/address')">
+      <div class="address_map" @click="$router.push({name:'address',params:{city:city}})">
         <i class="fa fa-map-marker"></i>
         <span>{{address}}</span>
         <i class="fa fa-sort-desc"></i>
@@ -18,8 +18,15 @@
 export default {
   name: "home",
   computed:{
+    // 获取到地址
     address(){
       return this.$store.getters.address
+    },
+    city(){
+      return (
+        this.$store.getters.location.addressComponent.city ||
+        this.$store.getters.location.addressComponent.province 
+      );
     }
   }
 };
